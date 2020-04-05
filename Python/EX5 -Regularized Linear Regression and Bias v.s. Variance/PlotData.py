a5db782bcb2d188d25c01f82e972ba37252659fb
Timothy Alex John <62374873+Timothy-John@users.noreply.github.com>
2020-04-05 17:35:10 +0530

    Delete PlotData.py

--- Python/EX5 -Regularized Linear Regression and Bias v.s. Variance/PlotData.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-#.....Plotting Data.....
-def PlotData(x,y):
-    plt.scatter(x,y)
-    plt.xlabel('Change in Water Level')
-    plt.ylabel('Amount of Water flowing out of Dam')
